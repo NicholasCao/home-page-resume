780c246ada6fb00298c405589848be43d9d8f3c6
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2018-11-25 15:33:47 +0800

    update .editorconfig

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ console.log('hello');
 import printMe from './print.js'
 
 if (module.hot) {
-  module.hot.accept('./print.js', function() {
-  console.log('Accepting the updated printMe module!');
-  printMe();
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    printMe();
   })
 }
 
