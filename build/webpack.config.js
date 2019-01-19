13ef09e6bc70e96c0afb132c4c4ea5467c754323
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-01-19 18:03:11 +0800

    update

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -36,7 +36,11 @@ module.exports = {
       {
         test: /\.less$/,
         loader: ['style-loader', 'css-loader', 'less-loader']
-      }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+      },
     ]
   },
   plugins: [
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import './less/normalize.less'
+import './less/main.less'
+import '../static/twitter.svg'
 import printMe from './print.js'
+
+
+
+
 
 if (module.hot) {
   module.hot.accept('./print.js', function () {
