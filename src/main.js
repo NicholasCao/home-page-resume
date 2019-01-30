a65ebd8d3d767d94ce5f975ecec3f1a50352a23b
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-01-30 19:16:22 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import './less/main.less'
 import printMe from './print.js'
 import defineReactive from './utils/defineReactive.js'
 
-data = {
-  
+var data = {
+  scrollingLock: false
 }
-
+// window.onload = (() => {
+//   window.onmousewheel = e => {
+//     e.stopPropagation()
+//     e.preventDefault()
+//     if (e.wheelDelta < -40) {
+//       // data.scrollingLock = true
+//       console.log(1)
+//     }
+//   }
+// })()
 
 
 
