2f2be34a16132354127e830e0769890e56d73f52
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-02-03 14:54:32 +0800

    feat: add random img

--- src/main.js
+++ src/main.js
@@ -7,8 +7,22 @@ import defineReactive from './utils/defineReactive.js'
 var data = {
   scrollingLock: false
 }
-
-const imgUrl = []
+const url = 'https://nicholas-profile.oss-cn-shenzhen.aliyuncs.com/'
+const imgs = [
+  'a.jpg',
+  'b.jpg',
+  'c.jpg',
+  'd.jpg',
+  'e.jpg'
+]
+var photos = document.getElementsByClassName('photo')
+var random1 = Math.floor(Math.random()*5)
+var random2 = Math.floor(Math.random()*5)
+while (random1 == random2){
+  var random2 = Math.floor(Math.random()*5)
+}
+photos[0].src = url + imgs[random1]
+photos[1].src = url + imgs[random2]
 // window.onload = (() => {
 //   window.onmousewheel = e => {
 //     e.stopPropagation()
